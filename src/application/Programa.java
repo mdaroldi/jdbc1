20875ca792b323b51600ff5006542f7bbfbbfbc0
Mauricio Aroldi <mauricio.aroldi@gmail.com>
2020-04-09 20:14:01 -0300

    Initial files

---
+++ src/application/Programa.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/db/DB.java
@@ -0,0 +1,13 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+	
+	
+}
